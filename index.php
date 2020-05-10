3e2db6a7bda087090b905020d5bf6c0f068f0307
Iman Ghasrfakhri <ghasrfakhri@gmail.com>
2020-05-10 18:23:47 +0430

    Add index.php file

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+//$a = 15;
+//if ($a > 10) {
+//    echo "Greater than 10";
+//    echo "Greater than 10";
+//} else {
+//    echo "Less than or Eq 10";
+//}
+//
+//
+//if ($a > 5) {
+//
+//}
+//
+//
+//if ($a > 5) {
+////fghfghgfhgh
+//} else if ($b > 5) {
+////dfgdfg
+//} else {
+//
+//}
+//
+//switch ($a) {
+//    case 1:
+//
+//        break;
+//    case 2:
+//
+//        break;
+//    case 3:
+//    case 4:
+//    case 5:
+//
+//        break;
+//    default:
+//
+//}
+
+//$n = 0;
+//while ($n > 1) {
+//    echo $n;
+//    echo "<br>";
+//    $n = $n / 2;
+//}
+//
+//$n = 0;
+//do {
+//    echo $n;
+//    echo "<br>";
+//    $n = $n / 2;
+//}while ($n > 1);
+
+//$i = $i + 1
+
+//for ($i = 1; $i <= 10; $i++) {
+//    echo $i;
+//    echo "<br>";
+//}
+
+$i = 10;
+$a = 4+5; // + - / * %
+$b = 9 % 2;
+$i += 2; // += /= -= *= %=
+$x = $i++ ;//++ --
+
+// >  <  >= <= ==  !=  ===  !==
+
+// && || !
+
+//var_dump( !true);
+
+if( $a > 10 || $b < 20){
+    //fgdfgfdg
+}
+
+
+$a = "10";
+if( $a == 10 ){  //   10 == "10" >>> True
+
+}
+
+if( $a === 10 ){  //   10 === "10" >>> False
+
+}
+//
+//$i = strpos("This is a test", "xyz");
+//
+//if($i === false){
+//    echo "Not found";
+//}else{
+//    echo "Found";
+//}
+
+
+
+
+
+
